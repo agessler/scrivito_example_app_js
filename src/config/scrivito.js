38a5ce1ddf7323271e75f8663b374fc439a84b1d
Alexander Pepper <alexander.pepper@infopark.de>
2020-06-24 11:25:25 +0200

    Allow to be framed and executed in a cloud UI preview deployment.

--- src/config/scrivito.js
+++ src/config/scrivito.js
@@ -1,6 +1,17 @@
 import * as Scrivito from "scrivito";
 
-const config = { tenant: process.env.SCRIVITO_TENANT };
+const config = {
+  tenant: process.env.SCRIVITO_TENANT,
+  unstable: {
+    trustedUiOrigins: [
+      "http://localhost:8090",
+      "https://*.d2gu55vvqejsqp.amplifyapp.com",
+      "https://*.netlify.app",
+      "https://*.netlify.com",
+      "https://dt5t498v8txnt.cloudfront.net",
+    ],
+  },
+};
 
 if (process.env.SCRIVITO_ORIGIN) {
   config.origin = process.env.SCRIVITO_ORIGIN;
